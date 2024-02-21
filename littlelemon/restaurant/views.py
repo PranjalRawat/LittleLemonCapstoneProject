16ed5bb4063f18d515859f0db5439e48b1fd4d10
qai33413 <qai33413@adobe.com>
2024-02-21 11:15:44 +0530

    Added User Authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,19 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Booking, Menu
 from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
